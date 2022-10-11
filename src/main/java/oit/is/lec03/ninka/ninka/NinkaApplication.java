cafcf364b0e339214c98cf10f9233bf169d8247d
e1b20111 <e1b20111@st.oit.ac.jp>
2022-10-11 14:23:02 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec03/ninka/ninka/NinkaApplication.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.ninka.ninka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec03/ninka/ninka/NinkaApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.ninka.ninka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
