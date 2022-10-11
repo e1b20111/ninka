b3aa874be64ed9fba15be5f4c9d07dd5a077427c
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-11 15:21:10 +0900

    ユーザー名の表示

--- src/main/java/oit/is/lec03/ninka/ninka/controller/Sample31Controller.java
+++ src/main/java/oit/is/lec03/ninka/ninka/controller/Sample31Controller.java
@@ -1,10 +1,10 @@
 package oit.is.lec03.ninka.ninka.controller;
 
-//import java.security.Principal;
+import java.security.Principal;
 
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +21,13 @@ public class Sample31Controller {
 
   @GetMapping("step1")
   public String sample31() {
+    return "sample31.html";
+  }
+
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
     return "sample31.html";
   }
 
